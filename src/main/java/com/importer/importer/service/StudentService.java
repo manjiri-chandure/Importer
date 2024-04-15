be42feab19d9cb686f503ba31a1accc180ce963d
manjiri-chandure <chanduremanjiri@gmail.com>
2024-04-15 16:52:53 +0530

    log get stored in database(log table) when exception come & student created successfully , primaryKey & primaryColumn removed from insert query in log.xml

--- src/main/java/com/importer/importer/service/StudentService.java
+++ src/main/java/com/importer/importer/service/StudentService.java
@@ -22,41 +22,60 @@ public class StudentService {
         StringBuilder responseBuilder = new StringBuilder();
         for(StudentCreationDto studentCreationDto : studentCreationDtos){
             HttpHeaders headers = new HttpHeaders();
-            String jwtToken = "eyJhbGciOiJIUzUxMiJ9.eyJSb2xlIjoiT0ZGSUNFX0FETUlOIiwiVXNlcklkIjoxLCJzdWIiOiJnYW5lc2hwYXRlbCIsImlhdCI6MTcxMzE3MDgyOCwiZXhwIjoxNzEzMTc4MDI4fQ.yLW7sQLBtQItEPHvbADbY3wRiqp_h7jMu23GJ9QvlNdgpkzgaqG1WMgYgL-COZROMFvXaxbHsU2ganFRKOE0Rw";
+            String jwtToken = "eyJhbGciOiJIUzUxMiJ9.eyJSb2xlIjoiT0ZGSUNFX0FETUlOIiwiVXNlcklkIjoxLCJzdWIiOiJnYW5lc2hwYXRlbCIsImlhdCI6MTcxMzE3ODY2OSwiZXhwIjoxNzEzMTg1ODY5fQ.9vbbK4uk8ywBu13LvUHTPCmyksYjH92vfZXoDsl5sZRSpJLlIwAAziHizcQUeTBnKt24xuLg3WK7O5P_DCt6Yw";
             headers.add("Authorization", "Bearer "+ jwtToken );
             headers.setAccept(List.of(MediaType.APPLICATION_JSON));
             HttpEntity<StudentCreationDto> entity = new HttpEntity<StudentCreationDto>(studentCreationDto, headers);
 
-
+            String status = "";
+            String responseBody = "";
+            int statusCode = 0;
+            String responseMessage = "";
             try {
                 ResponseEntity<String> responseEntity = new RestTemplate().exchange("http://localhost:8086/students", HttpMethod.POST, entity, String.class);
-
-                // Append status code and response body to the StringBuilder
                 responseBuilder.append("Status Code: ").append(responseEntity.getStatusCode()).append(System.lineSeparator());
-                String status = "";
-                Integer statusCode = 0;
-                String responceMessage = "";
-                status = responseEntity.getStatusCode().toString();
                 responseBuilder.append("Response Body: ").append(responseEntity.getBody()).append(System.lineSeparator());
+                status = responseEntity.getStatusCode().toString();
                 statusCode = Integer.parseInt(status.substring(0, status.indexOf(" ")));
-                responceMessage = status.substring(status.indexOf(" ")+1);
-                this.logRepository.addLog(studentCreationDto, statusCode, responceMessage);
-
+                responseMessage = status.substring(status.indexOf(" ")+1);
+                responseBody = "Student Created Successfully";
 
             } catch (HttpClientErrorException.BadRequest ex) {
-                // Handle 400 Bad Request error
-                responseBuilder.append("Error: ").append(ex.getStatusCode()).append(" - ").append(ex.getResponseBodyAsString()).append(System.lineSeparator());
-            }
-            catch (HttpClientErrorException ex) {
+
+                status = ex.getStatusCode().toString();
+                statusCode = ex.getStatusCode().value();
+                responseMessage = status.substring(status.indexOf(" ")+1);
+                if(!ex.getResponseBodyAsString().isEmpty())
+                    responseBody = ex.getResponseBodyAsString();
+            } catch (HttpClientErrorException ex) {
                 // Handle other 4xx errors
-                responseBuilder.append("Error: ").append(ex.getStatusCode()).append(System.lineSeparator());
+                status = ex.getStatusCode().toString();
+                statusCode = ex.getStatusCode().value();
+                responseMessage = status.substring(status.indexOf(" ")+1);
+                if(!ex.getResponseBodyAsString().isEmpty())
+                    responseBody = ex.getResponseBodyAsString();
 
             } catch (Exception ex) {
-                // Handle other exceptions
                 responseBuilder.append("Error: ").append(ex.getMessage()).append(System.lineSeparator());
+                statusCode = 500;
+                responseMessage = "Internal Server Error";
+            }
+            finally {
+                if(!responseBody.isEmpty()){
+                    responseBuilder.append("Response: ").append(statusCode).append(" ").append(responseMessage).append(" - ").append(responseBody).append(System.lineSeparator());
+                    logRequest(studentCreationDto, statusCode, responseMessage + ": "+ responseBody);
+                }
+                else{
+                    responseBuilder.append("Response: ").append(statusCode).append(" ").append(responseMessage);
+                    logRequest(studentCreationDto, statusCode, responseMessage);
+                }
             }
         }
 
         return responseBuilder.toString();
+    }
+
+    private void logRequest(StudentCreationDto studentCreationDto, int statusCode, String message) {
+        logRepository.addLog(studentCreationDto, statusCode, message);
     }
 }
