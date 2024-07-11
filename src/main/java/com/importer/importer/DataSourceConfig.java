89d92a7f17390fe0c935c6e6e700e9c8704e0315
manjiri-chandure <chanduremanjiri@gmail.com>
2024-07-11 11:58:21 +0530

    properties which are sensitive and encrypted in application.properties are get decrypted and set through a property config and correct name added to functions

--- src/main/java/com/importer/importer/DataSourceConfig.java
+++ src/main/java/com/importer/importer/DataSourceConfig.java
@@ -3,13 +3,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 import java.io.IOException;
 
 @Configuration
-//@DependsOn("propertyConfig")
+@DependsOn("propertyConfig")
 public class DataSourceConfig {
 
 
@@ -22,16 +23,10 @@ public class DataSourceConfig {
     @Value("${spring.datasource.password}")
     private String dataBasePassword;
 
-    @Autowired
-    private KMSUtil kmsUtil;
 
     @Bean
     public DataSource dataSource() throws IOException {
-       // String dbPassword = environment.getProperty("spring.datasource.password");
-        boolean isEncrypted = checkDbPasswordIsEncrypted(dataBasePassword);
-        if(isEncrypted){
-          dataBasePassword = decryptEncryptedPassword(dataBasePassword);
-        }
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl(dataBaseUrl);
@@ -40,13 +35,6 @@ public class DataSourceConfig {
         return dataSource;
     }
 
-    private boolean checkDbPasswordIsEncrypted(String dataBasePassword) {
-        return dataBasePassword.startsWith("ENC(") & dataBasePassword.endsWith(")");
-    }
 
-    private String decryptEncryptedPassword(String dataBasePassword) {
-        String encryptedPassword = dataBasePassword.substring(4,dataBasePassword.length()-1);//last character ')' get excluded
-        return kmsUtil.kmsDecrypt(encryptedPassword);
-    }
 
 }
--- src/main/java/com/importer/importer/PropertyConfig.java
+++ src/main/java/com/importer/importer/PropertyConfig.java
@@ -1,29 +1,52 @@
-//package com.importer.importer;
-//import jakarta.annotation.PostConstruct;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.env.ConfigurableEnvironment;
-//import org.springframework.core.env.PropertiesPropertySource;
-//import java.util.Properties;
-//
-//@Configuration
-//public class PropertyConfig {
-//
-//    @Autowired
-//    private ConfigurableEnvironment env;
-//
-//    @Value("${database.encrypted-password}")
-//    private String encryptedDatabasePassword;
-//
-//    @Autowired
-//    private KMSUtil kmsUtil;
-//
-//    @PostConstruct
-//    public void setProperty() {
-//        Properties props = new Properties();
-//        props.put("spring.datasource.password", kmsUtil.kmsDecrypt(encryptedDatabasePassword));
-//       env.getPropertySources().addFirst(new PropertiesPropertySource("customProperty", props));
-//   }
-//
-//}
+package com.importer.importer;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertiesPropertySource;
+import java.util.Properties;
+
+@Configuration
+public class PropertyConfig {
+
+    @Autowired
+    private ConfigurableEnvironment env;
+
+    @Value("${spring.datasource.password}")
+    private String databasePassword;
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Autowired
+    private KMSUtil kmsUtil;
+
+    @PostConstruct
+    public void setProperty() {
+
+        if(checkPropertyIsEncrypted(databasePassword)){
+            databasePassword = decryptEncryptedProperty(databasePassword);
+        }
+
+        if(checkPropertyIsEncrypted(jwtSecret)){
+            jwtSecret = decryptEncryptedProperty(jwtSecret);
+        }
+
+        Properties props = new Properties();
+        //properties get replace here
+        props.put("spring.datasource.password", databasePassword);
+        props.put("jwt.secret",jwtSecret);
+        env.getPropertySources().addFirst(new PropertiesPropertySource("customProperty", props));
+   }
+
+    private boolean checkPropertyIsEncrypted(String property) {
+        return property.startsWith("ENC(") & property.endsWith(")");
+    }
+
+    private String decryptEncryptedProperty(String property) {
+        String encryptedProperty = property.substring(4, property.length()-1);//last character ')' get excluded
+        return kmsUtil.kmsDecrypt(encryptedProperty);
+    }
+
+}
